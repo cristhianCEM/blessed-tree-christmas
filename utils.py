4ee758b6213647dda788bafdb3850f4f0ee10ff1
cristhianCEM <cristianbcer27@gmail.com>
2023-12-18 12:12:04 -0600

    feat: add measure_time

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 def echo_python2(text):
     """Función 'echo' para Python 2: Imprime sin añadir salto de línea y vacía el buffer."""
@@ -14,3 +15,15 @@ if sys.version_info[0] < 3:
     echo = echo_python2
 else:
     echo = echo_python3
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        function_name = func.__name__
+        print(f"- Iniciando {function_name} -")
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        elapsed_time = end - start
+        print(f"Tiempo transcurrido en {function_name}: {elapsed_time:.6f} segundos")
+        return result
+    return wrapper
