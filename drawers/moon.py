598bd280c2735208739c3319df0cad7506061de1
cristhianCEM <64231401+cristhianCEM@users.noreply.github.com>
2023-12-22 00:00:36 -0600

    feat: create translate moon

--- drawers/moon.py
+++ drawers/moon.py
@@ -1,6 +1,6 @@
 from utils import echo
 
-MOON_MIDDLE = [
+BIG_MOON = [
     "#########___---___",
     "######.-'         '-.",
     "####./   ()      .-. \.",
@@ -18,10 +18,100 @@ MOON_MIDDLE = [
     "######### ''---''"
 ]
 
-def draw_moon(terminal, x, y):
-    for i in range(len(MOON_MIDDLE)):
-        string = MOON_MIDDLE[i]
+TRANSLATE_MOON = [
+    [
+    "   _..._",
+    " .:::::::.",
+    ":::::::::::",
+    ":::::::::::",
+    "`:::::::::'",
+    "  `':::''",
+    ], [
+    "   _..._",
+    " .::::. `.",
+    ":::::::.  :",
+    "::::::::  :",
+    "`::::::' .'",
+    "  `'::'-'",
+    ], [
+    "   _..._",
+    " .::::  `.",
+    "::::::    :",
+    "::::::    :",
+    "`:::::   .'",
+    "  `'::.-'",
+    ], [
+    "   _..._",
+    " .::'   `.",
+    ":::       :",
+    ":::       :",
+    "`::.     .'",
+    "  `':..-'",
+    ], [
+    "   _..._",
+    " .'     `.",
+    ":         :",
+    ":         :",
+    "`.       .'",
+    "  `-...-'",
+    ], [
+    "   _..._",
+    " .'   `::.",
+    ":       :::",
+    ":       :::",
+    "`.     .::'",
+    "  `-..:''",
+    ], [
+    "   _..._",
+    " .'  ::::.",
+    ":    ::::::",
+    ":    ::::::",
+    "`.   :::::'",
+    "  `-.::''",
+    ], [
+    "   _..._",
+    " .' .::::.",
+    ":  ::::::::",
+    ":  ::::::::",
+    "`. '::::::'",
+    "  `-.::''",
+    ], [
+    "   _..._",
+    " .:::::::.",
+    ":::::::::::",
+    ":::::::::::",
+    "`:::::::::'",
+    "  `':::''",
+    ]
+]
+TRANSLATE_MOON_LEN = len(TRANSLATE_MOON)
+TRANSLATE_MOON_TIMING = [10, 10, 10, 10, 10, 10, 10, 10, 10]
+moon_index = 0
+moon_counter = 0
+
+def draw_big_moon(terminal, x, y):
+    for i in range(len(BIG_MOON)):
+        string = BIG_MOON[i]
         cantidad = string.count('#')
         string = string.replace('#', '')
         echo(terminal.move_xy(x + cantidad, y + i))
+        echo(terminal.on_black(terminal.bright_white(string)))
+
+def draw_translate_moon(terminal, x, y):
+    global moon_index, moon_counter
+    timming = TRANSLATE_MOON_TIMING[moon_index]
+    if (moon_counter == 0):
+        moon_index += 1
+        if (moon_index >= TRANSLATE_MOON_LEN):
+            moon_index = 0
+    if (moon_counter == timming):
+        moon_counter = 0
+    else:
+        moon_counter += 1
+    moon = TRANSLATE_MOON[moon_index]
+    for i in range(len(moon)):
+        string = moon[i]
+        # cantidad = string.count(':')
+        # string = string.replace(':', '')
+        echo(terminal.move_xy(x, y + i))
         echo(terminal.on_black(terminal.bright_white(string)))
