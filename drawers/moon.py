180e568494044e3a0d5a60e586790742264f6a7c
cristhianCEM <64231401+cristhianCEM@users.noreply.github.com>
2023-12-22 17:13:52 -0600

    fix: full moon

--- drawers/moon.py
+++ drawers/moon.py
@@ -1,125 +1,36 @@
 from utils import echo
 import math
 
-BIG_MOON = [
+FULL_MOON = [
     "#########___---___",
     "######.-'         '-.",
     "####./   ()      .-. \.",
-    "###/   o    .   (   )  \ ",
+    "###/   o    .   (:::)  \ ",
     "##/ .            '-'    \ ",
-    "#| ()    .  O         .  |",
+    "#| ()   ::  O         .  |",
     "|                         |",
     "|    o           ()       |",
-    "|       .--.          O   |",
-    "#| .   |    |            |",
-    "##\    `.__.'    o   .  /",
+    "|       .::.          O   |",
+    "#| .   ::::::            |",
+    "##\     ::::     ::  .  /",
     "###\                   /",
     "####`\  o    ()      /'",
     "######`--_       _--'",
     "######### ''---''"
 ]
 
-TRANSLATE_MOON = [
-    [
-    "   _..._",
-    " .:::::::.",
-    ":::::::::::",
-    ":::::::::::",
-    "`:::::::::'",
-    "  `':::''",
-    ], [
-    "   _..._",
-    " .::::. `.",
-    ":::::::.  :",
-    "::::::::  :",
-    "`::::::' .'",
-    "  `'::'-'",
-    ], [
-    "   _..._",
-    " .::::  `.",
-    "::::::    :",
-    "::::::    :",
-    "`:::::   .'",
-    "  `'::.-'",
-    ], [
-    "   _..._",
-    " .::'   `.",
-    ":::       :",
-    ":::       :",
-    "`::.     .'",
-    "  `':..-'",
-    ], [
-    "   _..._",
-    " .'     `.",
-    ":         :",
-    ":         :",
-    "`.       .'",
-    "  `-...-'",
-    ], [
-    "   _..._",
-    " .'   `::.",
-    ":       :::",
-    ":       :::",
-    "`.     .::'",
-    "  `-..:''",
-    ], [
-    "   _..._",
-    " .'  ::::.",
-    ":    ::::::",
-    ":    ::::::",
-    "`.   :::::'",
-    "  `-.::''",
-    ], [
-    "   _..._",
-    " .' .::::.",
-    ":  ::::::::",
-    ":  ::::::::",
-    "`. '::::::'",
-    "  `-.::''",
-    ], [
-    "   _..._",
-    " .:::::::.",
-    ":::::::::::",
-    ":::::::::::",
-    "`:::::::::'",
-    "  `':::''",
-    ]
-]
-TRANSLATE_MOON_LEN = len(TRANSLATE_MOON)
-TRANSLATE_MOON_TIMING = [10, 10, 10, 10, 10, 10, 10, 10, 10]
-moon_index = 0
-moon_counter = 0
 moon_x = 0
 moon_y = 0
 
-def draw_big_moon(terminal, x, y):
-    for i in range(len(BIG_MOON)):
-        string = BIG_MOON[i]
-        cantidad = string.count('#')
-        string = string.replace('#', '')
-        echo(terminal.move_xy(x + cantidad, y + i))
-        echo(terminal.on_black(terminal.bright_white(string)))
-
-def draw_translate_moon(terminal, reset: bool = False):
-    global moon_index, moon_counter, moon_x, moon_y
+def draw_full_moon(terminal, reset: bool = False):
+    global moon_x, moon_y
     if (reset):
-        moon_index = 0
-        moon_counter = 0
         moon_x = math.floor(terminal.width / 10)
         moon_y = math.floor(terminal.height / 10)
-    timming = TRANSLATE_MOON_TIMING[moon_index]
-    if (moon_counter == 0):
-        moon_index += 1
-        if (moon_index >= TRANSLATE_MOON_LEN):
-            moon_index = 0
-    if (moon_counter == timming):
-        moon_counter = 0
-    else:
-        moon_counter += 1
-    moon = TRANSLATE_MOON[moon_index]
-    for i in range(len(moon)):
-        string = moon[i]
-        # cantidad = string.count(':')
-        # string = string.replace(':', '')
-        echo(terminal.move_xy(moon_x, moon_y + i))
-        echo(terminal.on_black(terminal.bright_white(string)))
+    echo(terminal.on_black(terminal.bright_white('')))
+    for i in range(len(FULL_MOON)):
+        string = FULL_MOON[i]
+        cantidad = string.count('#')
+        string = string.replace('#', '')
+        echo(terminal.move_xy(moon_x + cantidad, moon_y + i))
+        echo(string)
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 
 from utils import echo, get_text_formatted
 from drawers.starsky import draw_starsky
-from drawers.moon import draw_translate_moon
+from drawers.moon import draw_full_moon
 from drawers.tree import draw_base_star, draw_base_tree, draw_fairy_lights
 
 MESSAGE = 'Feliz Navidad!'
@@ -29,7 +29,7 @@ def setup(terminal):
     echo(terminal.on_black(terminal.clear()))
     echo(terminal.move_xy(0, 0))
     draw_starsky(terminal, True)
-    draw_translate_moon(terminal, True)
+    draw_full_moon(terminal, True)
     star_x = math.floor(terminal.width / 2)
     star_y = 1
     draw_base_star(terminal, star_x, star_y)
@@ -40,7 +40,7 @@ def setup(terminal):
 
 def loop(terminal):
     draw_starsky(terminal, False)
-    draw_translate_moon(terminal, False)
+    draw_full_moon(terminal, False)
     draw_fairy_lights(terminal)
 
 
