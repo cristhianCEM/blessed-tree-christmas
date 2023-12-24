61a2d14b16b4016948ffbde9e57da8bf2af4c7be
cristhianCEM <64231401+cristhianCEM@users.noreply.github.com>
2023-12-23 21:53:06 -0600

    fix: document code

--- utils/colors.py
+++ utils/colors.py
@@ -1,21 +1,51 @@
 import colorsys
+from typing import List
 
 
-def rgba_to_hsl(rgba):
-    r, g, b, a = rgba
-    h, l, s = colorsys.rgb_to_hls(r / 255, g / 255, b / 255)
-    return [h * 360, s * 100, l * 100, a]
-
-
-def hsl_to_rgba(hsl):
-    h, s, l, a = hsl
-    r, g, b = colorsys.hls_to_rgb(h / 360, l / 100, s / 100)
-    return [int(r * 255), int(g * 255), int(b * 255), a]
-
-
-def additive_mix_rgba_colors(base: list, added: list):
+def rgba_to_hsl(r: int, g: int, b: int, alpha: float = 1) -> List[float]:
     """
-        Mezcla dos colores RGB de forma aditiva
+    Converts an RGBA color to HSL color space.
+
+    Args:
+        r (int): The red component of the color (0-255).
+        g (int): The green component of the color (0-255).
+        b (int): The blue component of the color (0-255).
+        alpha (float, optional): The alpha (opacity) value of the color (0-1). Defaults to 1.
+
+    Returns:
+        List[float]: The HSL representation of the color [h, s, l, alpha].
+    """
+    h, l, s = colorsys.rgb_to_hls(r / 255, g / 255, b / 255)
+    return [h * 360, s * 100, l * 100, alpha]
+
+
+def hsl_to_rgba(h: float, s: float, l: float, alpha: float = 1) -> List[int]:
+    """
+    Converts an HSL color to RGBA color space.
+
+    Args:
+        h (float): The hue component of the color (0-360).
+        s (float): The saturation component of the color (0-100).
+        l (float): The lightness component of the color (0-100).
+        alpha (float, optional): The alpha (opacity) value of the color (0-1). Defaults to 1.
+
+    Returns:
+        List[int]: The RGBA representation of the color [r, g, b, alpha].
+    """
+    r, g, b = colorsys.hls_to_rgb(h / 360, l / 100, s / 100)
+    return [int(r * 255), int(g * 255), int(b * 255), alpha]
+
+
+def additive_mix_rgba_colors(base: List[int], added: List[int]) -> List[int]:
+    """
+    Mixes two RGB colors additively.
+
+    Args:
+        base (List[int]): The base color in RGBA format.
+        added (List[int]): The color to be added in RGBA format.
+
+    Returns:
+        List[int]: The resulting color after the additive mixing in RGBA format.
     """
     alpha_mix = 1 - (1 - added[3]) * (1 - base[3])
     if alpha_mix == 0:
@@ -29,10 +59,20 @@ def additive_mix_rgba_colors(base: list, added: list):
     return rgb_mix
 
 
-def hsl_mix_rgba_colors(color1: list, color2: list):
-    hsl1 = rgba_to_hsl(color1)
-    hsl2 = rgba_to_hsl(color2)
+def hsl_mix_rgba_colors(base: List[int], added: List[int]) -> List[int]:
+    """
+    Mixes two RGB colors using HSL color space.
+
+    Args:
+        base (List[int]): The base color in RGBA format.
+        added (List[int]): The color to be added in RGBA format.
+
+    Returns:
+        List[int]: The resulting color after the HSL mixing in RGBA format.
+    """
+    hsl1 = rgba_to_hsl(*base)
+    hsl2 = rgba_to_hsl(*added)
     mixed_hsl = [
         (hsl1[i] + hsl2[i]) / 2 for i in range(3)
-    ] + [1 - (1 - color1[3]) * (1 - color2[3])]  # mezcla de alfa
-    return hsl_to_rgba(mixed_hsl)
+    ] + [1 - (1 - base[3]) * (1 - added[3])]
+    return hsl_to_rgba(*mixed_hsl)
