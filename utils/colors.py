40ea2e11d795579d3e58f3825edd3c3dc14c8a48
cristhianCEM <64231401+cristhianCEM@users.noreply.github.com>
2023-12-23 21:37:40 -0600

    fix: separate functions for utils

--- utils/colors.py
+++ utils/colors.py
@@ -1,5 +1,19 @@
+import colorsys
 
-def additive_mix_RGB_colors(base: list, added: list):
+
+def rgba_to_hsl(rgba):
+    r, g, b, a = rgba
+    h, l, s = colorsys.rgb_to_hls(r / 255, g / 255, b / 255)
+    return [h * 360, s * 100, l * 100, a]
+
+
+def hsl_to_rgba(hsl):
+    h, s, l, a = hsl
+    r, g, b = colorsys.hls_to_rgb(h / 360, l / 100, s / 100)
+    return [int(r * 255), int(g * 255), int(b * 255), a]
+
+
+def additive_mix_rgba_colors(base: list, added: list):
     """
         Mezcla dos colores RGB de forma aditiva
     """
@@ -13,3 +27,12 @@ def additive_mix_RGB_colors(base: list, added: list):
     ]
     rgb_mix.append(alpha_mix)
     return rgb_mix
+
+
+def hsl_mix_rgba_colors(color1: list, color2: list):
+    hsl1 = rgba_to_hsl(color1)
+    hsl2 = rgba_to_hsl(color2)
+    mixed_hsl = [
+        (hsl1[i] + hsl2[i]) / 2 for i in range(3)
+    ] + [1 - (1 - color1[3]) * (1 - color2[3])]  # mezcla de alfa
+    return hsl_to_rgba(mixed_hsl)
