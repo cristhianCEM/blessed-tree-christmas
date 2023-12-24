a626434538f36cfa006f4d9851716d40ea6b9c33
cristhianCEM <64231401+cristhianCEM@users.noreply.github.com>
2023-12-24 00:44:28 -0600

    feat: check_resize console

--- drawers/map_3d.py
+++ drawers/map_3d.py
@@ -1,5 +1,6 @@
 import numpy as np
 from utils.colors import hsl_mix_rgba_colors
+from utils.console import echo, rgb_color
 
 
 class ConsoleMap3d(object):
@@ -7,7 +8,7 @@ class ConsoleMap3d(object):
     width = None
     height = None
     depth = None
-    map = None
+    space = None
 
     def __init__(self, terminal, depth: int = 0):
         self.terminal = terminal
@@ -18,7 +19,33 @@ class ConsoleMap3d(object):
         self.width = width
         self.height = height
         self.depth = depth
-        self.map = np.zeros((self.width, self.height, self.depth), dtype=int)
+        self.space = np.zeros((self.width, self.height, self.depth), dtype=int)
 
-    def resize(self):
-        self.resize(self.terminal.width, self.terminal.height, self.depth)
+    def check_resize(self):
+        new_width, new_height = self.terminal.width, self.terminal.height
+        if self.width != new_width or self.height != new_height:
+            self.resize_to(new_width, new_height, self.depth)
+
+    def set_point(self, x: int, y: int, z: int, caracter: str, color: tuple = (0, 0, 0, 0)):
+        self.space[x][y][z] = [caracter, color]
+
+    def del_point(self, x: int, y: int, z: int):
+        self.space[x][y][z] = 0
+
+    def draw(self):
+        for y in range(self.height):
+            text = ''
+            echo(self.terminal.move_xy(0, y))
+            for x in range(self.width):
+                position = self.space[x][y]
+                caracter = ' '
+                color = None
+                for z in range(self.depth):
+                    item = position[z]
+                    if item != 0:
+                        caracter, color = item
+                if color is None:
+                    text += caracter
+                else:
+                    text += rgb_color(self.terminal, color, caracter)
+            echo(self.terminal.on_black(text))
