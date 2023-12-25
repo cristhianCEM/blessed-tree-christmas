94cb06ec349209d25c215d60c2437570a50e2f72
cristhianCEM <64231401+cristhianCEM@users.noreply.github.com>
2023-12-24 18:47:37 -0600

    fix: draw_full_moon

--- drawers/moon.py
+++ drawers/moon.py
@@ -1,7 +1,6 @@
-from utils.console import echo
 import math
 
-FULL_MOON = [
+MOON = [
     "#########___---___",
     "######.-'         '-.",
     "####./   ()      .-. \.",
@@ -16,21 +15,23 @@ FULL_MOON = [
     "###\                   /",
     "####`\  o    ()      /'",
     "######`--_       _--'",
-    "######### ''---''"
+    "##########''---''"
 ]
-
+MOON_DEPTH = 1
+MOON_COLOR = (255, 255, 255, 1)
 moon_x = 0
 moon_y = 0
 
-def draw_full_moon(terminal, reset: bool = False):
+
+def draw_full_moon(scene, reset: bool = False):
     global moon_x, moon_y
     if (reset):
-        moon_x = math.floor(terminal.width / 10)
-        moon_y = math.floor(terminal.height / 10)
-    echo(terminal.on_black(terminal.bright_white('')))
-    for i in range(len(FULL_MOON)):
-        string = FULL_MOON[i]
+        moon_x = math.floor(scene.width / 10)
+        moon_y = math.floor(scene.height / 10)
+    for i in range(len(MOON)):
+        string = MOON[i]
         cantidad = string.count('#')
         string = string.replace('#', '')
-        echo(terminal.move_xy(moon_x + cantidad, moon_y + i))
-        echo(string)
+        m_x = moon_x + cantidad
+        m_y = moon_y + i
+        scene.set_message(m_x, m_y, MOON_DEPTH, string, MOON_COLOR)
